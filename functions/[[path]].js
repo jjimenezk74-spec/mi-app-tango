3ccc1062fc72bbde3ae0f902cb52c2eb259364fd
jjimenezk74-spec <jjimenezk74@gmail.com>
2026-03-12 21:00:06 -0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,1 +1,9 @@
-
+export async function onRequest(context) {
+  const { request, env } = context;
+  try {
+    const worker = await import("../src/worker/index.js"); 
+    return await worker.default.fetch(request, env);
+  } catch (e) {
+    return new Response("Error interno: " + e.message, { status: 500 });
+  }
+}
